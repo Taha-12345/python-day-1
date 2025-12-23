cdf62737de2e0e1900d5428a1a96c6fb5214fc8c
Taha-12345 <164293775+Taha-12345@users.noreply.github.com>
2025-12-24 00:34:25 +0500

    Add files via upload

--- day1_basics.py
+++ day1_basics.py
@@ -1,3 +1,14 @@
-print("Hello, World!")
-print("My Python journey has started!")
-print("Day 1 completed successfully")
+print("Hello World!")
+print("My name is Muhammad Taha")
+print("I am starting my Python journey today.")
+
+#Print age using a variable
+age = 21
+print("I am ",age," years old")
+
+#Print a sentence using multiple variables
+name = "Muhammad Taha"
+age = 21
+goal = "Python Developer"
+
+print("My name is" ,name, "and my age is" ,age, "years. My goal is to be a" ,goal)
